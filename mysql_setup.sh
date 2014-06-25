30eb7f7ec236ac35c82ab5fddb987f88689419d0
jake <bigjake2s@gmail.com>
2014-06-25 10:49:24 -0400

    Moved things around to run the MySQL setup during initial boot instead of on image creation (creates support for hosting /var/lib/mysql externally)

--- mysql_setup.sh
+++ mysql_setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-/usr/bin/mysqld_safe > /dev/null 2>&1 &
+# /usr/bin/mysqld_safe > /dev/null 2>&1 &
 
 RET=1
 while [[ RET -ne 0 ]]; do
 	echo "=> Waiting for confirmation of MySQL service startup"
 	sleep 5
-	mysql -uroot -e "status" > /dev/null 2>&1
+	/usr/bin/mysql -uroot -e "status" > /dev/null 2>&1
 	RET=$?
 done
 
 /usr/bin/mysql -uroot -e "CREATE USER 'root'@'%' IDENTIFIED BY ''"
 /usr/bin/mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION"
-/usr/bin/mysqladmin -uroot shutdown
+# /usr/bin/mysqladmin -uroot shutdown
